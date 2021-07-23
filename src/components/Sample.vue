ac89cbefb0915f131180dc2f693c02840d685423
Andrew Dagdag <aedagdag@itemhound.com>
2021-07-23 16:14:25 +0800

    Andrew: initial commit

---
+++ src/components/Sample.vue
@@ -0,0 +1,31 @@
+<template>
+  <h1 v-if="sampleProp && sampleProp.length">
+    {{ sampleProp }}
+  </h1>
+</template>
+<script>
+export default {
+  name: 'sample',
+  props: {
+    sampleProp: {
+      type: String,
+      default: 'Yeeee'
+    }
+  }
+}
+</script>
+<style lang="scss">
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+
+a {
+  color: #42b983;
+}
+</style>
