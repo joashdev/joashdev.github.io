c87d7436f826442a897f8b71f8f12d8b5e7fd45e
Kent Zamudio <kent.zamudio@itemhound.com>
2021-08-04 16:51:48 +0800

    Modified about section

--- src/components/About.vue
+++ src/components/About.vue
@@ -1,7 +1,9 @@
 <template>
   <section id="about" class="section">
     <h1 class="is-family-monospace is-white is-size-1">#ABOUT</h1>
-    <h1 class="is-family-primary has-text-weight-bold is-white is-size-2 mx-3">Education</h1>
+    <h1 class="is-family-primary has-text-weight-bold is-white is-size-2 mx-3 pt-3">
+      Education
+    </h1>
     <div>
       <div class="columns is-multiline is-flex is-justify-content-space-evenly">
         <CompactCard
@@ -13,7 +15,9 @@
         />
       </div>
     </div>
-    <h1 class="is-family-primary has-text-weight-bold is-white is-size-2 mx-3">Skills</h1>
+    <h1 class="is-family-primary has-text-weight-bold is-white is-size-2 mx-3 pt-3">
+      Skills
+    </h1>
     <div v-for="(category, index) in skills" :key="index">
       <h1 class="is-family-primary has-text-centered is-white is-size-3">
         {{ category.name }}
@@ -28,7 +32,17 @@
         />
       </div>
     </div>
-    <h1 class="is-family-primary has-text-weight-bold is-white is-size-2 mx-3">Joash</h1>
+    <h1 class="is-family-primary has-text-weight-bold is-white is-size-2 mx-3 pt-3">
+      Joash
+    </h1>
+    <div class="container">
+      <p class="subtitle">I was once inspired by a song that I heard on TV, and this time, allow me to share that song to you all:</p>
+      <p class="subtitle px-6 mx-6 has-text-centered is-italic">
+        Stori opmalay serchinpordaray Keepkeepsapodami, Soroweemasow Kosisisarong
+        Rililabsmahkempani, Hismodanaman endisismordanlab Darisondadaskaywisbloo
+        Kosemrolinin Kosamganagen Entuhemayjaskenbetru (Stori opmalay by Marian Rivera, 2018)
+      </p>
+    </div>
   </section>
 </template>
 
